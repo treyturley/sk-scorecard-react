6c92183eff656abb6dda862f4575464b3ace8107
treyturley <81778542+treyturley@users.noreply.github.com>
2022-09-02 22:55:39 -0500

    removes bootstrap from player setup and uses css instead. adds new background.

--- src/components/PlayerSetup.js
+++ src/components/PlayerSetup.js
@@ -1,5 +1,5 @@
-import { FormControl, Row, Col, Container, Button, Form } from "react-bootstrap";
-import backgroundVideo from '../video/Sea.mp4'
+import backgroundVideo from '../video/pirate-flag-waving.mp4'
+// import backgroundVideo from '../video/Sea.mp4'
 import '../styles/PlayerSetup.css'
 
 function PlayerSetupForm(props) {
@@ -14,65 +14,54 @@ function PlayerSetupForm(props) {
   }
 
   return (
-        <Container>
-          <Row className="mb-3 justify-content-center">
-            <Col></Col>
-            <Col sm={4}><h1>Player Setup</h1></Col>
-            <Col></Col>
-          </Row>
-          <Form onSubmit={handleSubmit}>
-            {/* TODO: given that we repeat the row element 4 times this
-          may be worth extracting into a function or reducing in some other way */}
-            <Row sm={3} className="mb-3 justify-content-center">
-              <Col>
-                <FormControl
-                  type="text"
-                  name="Player 1"
-                  placeholder="Player 1"
-                  value={players[0] || ""}
-                  onChange={handleChange(0)} />
-              </Col>
-            </Row>
-            <Row sm={3} className="mb-3 justify-content-center">
-              <Col>
-                <FormControl
-                  type="text"
-                  name="Player 2"
-                  placeholder="Player 2"
-                  value={players[1] || ""}
-                  onChange={handleChange(1)} />
-              </Col>
-            </Row>
-            <Row sm={3} className="mb-3 justify-content-center">
-              <Col>
-                <FormControl
-                  type="text"
-                  name="Player 3"
-                  placeholder="Player 3"
-                  value={players[2] || ""}
-                  onChange={handleChange(2)} />
-              </Col>
-            </Row>
-            <Row sm={3} className="mb-3 justify-content-center">
-              <Col>
-                <FormControl
-                  type="text"
-                  name="Player 4"
-                  placeholder="Player 4"
-                  value={players[3] || ""}
-                  onChange={handleChange(3)} />
-              </Col>
-            </Row>
-            <Row xs={1} sm={3} lg={3} className="mb-3 justify-content-center">
-              <Col></Col>
-              <Col><Button type="submit">Start Game</Button></Col>
-              <Col></Col>
-            </Row>
-          </Form>
-            <video id='video' autoPlay loop muted>
-              <source src={backgroundVideo} type='video/mp4'></source>
-            </video>
-        </Container>
+    <>
+      <div className="my-container">
+        <form onSubmit={handleSubmit}>
+
+
+          <input
+            type="text"
+            name="Player 1"
+            placeholder="Player 1"
+            value={players[0] || ""}
+            onChange={handleChange(0)} />
+
+
+          <input
+            type="text"
+            name="Player 2"
+            placeholder="Player 2"
+            value={players[1] || ""}
+            onChange={handleChange(1)} />
+
+
+          <input
+            type="text"
+            name="Player 3"
+            placeholder="Player 3"
+            value={players[2] || ""}
+            onChange={handleChange(2)} />
+
+
+          <input
+            type="text"
+            name="Player 4"
+            placeholder="Player 4"
+            value={players[3] || ""}
+            onChange={handleChange(3)} />
+
+          <div className="form-button">
+            <button type="submit">Set Sail</button>
+          </div>
+
+        </form>
+
+      </div>
+
+      <video id='video' autoPlay loop muted>
+        <source src={backgroundVideo} type='video/mp4'></source>
+      </video>
+    </>
   )
 }
 
