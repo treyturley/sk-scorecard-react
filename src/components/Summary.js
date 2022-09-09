df2776bcaacdf41e665cd4f73917c91fdbf8abb6
treyturley <81778542+treyturley@users.noreply.github.com>
2022-09-08 20:20:06 -0500

    adds ordered player list based on score

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,15 +1,80 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import "../styles/Summary.css"
 
-function Summary({playerTotals, scorecard}){
+function Summary({ playerTotals, scorecard, setGameComplete }) {
+
+  function onClickBackToScores() {
+    setGameComplete(false);
+  }
+
+  function getSortedPlayers() {
+
+    let output = null;
+    let sortedPlayers = [...playerTotals];
+
+    // TODO: Need to check for ties to ensure correct sizing gets applied
+
+    if (sortedPlayers.length >= 3) {
+      output = sortedPlayers
+        .sort((a, b) => a.total < b.total ? 1 : -1)
+        .map((player, index) => {
+          if (index === 0) {
+            //first
+            return <li className='first-place' key={player.playerName}>{player.playerName} : {player.total}</li>
+          } else if (index === 1) {
+            //second
+            return <li className='second-place' key={player.playerName}>{player.playerName} : {player.total}</li>
+          } else if (index === 2) {
+            //third
+            return <li className='third-place' key={player.playerName}>{player.playerName} : {player.total}</li>
+          } else if (index > 2) {
+            return <li className='other-places' key={player.playerName}>{player.playerName} : {player.total}</li>
+          }
+          return null;
+        });
+
+    } else if (sortedPlayers.length === 2) {
+      // handle two player game
+      output = sortedPlayers
+        .sort((a, b) => a.total < b.total ? 1 : -1)
+        .map((player, index) => {
+          if (index === 0) {
+            //first
+            return <li className='first-place' key={player.playerName}>{player.playerName} : {player.total}</li>
+          } else if (index === 1) {
+            //second
+            return <li className='second-place' key={player.playerName}>{player.playerName} : {player.total}</li>
+          }
+          return null;
+        });
+    } else {
+      // in theory game cant be played with anything less that two players
+      console.error(`Error occured in function getSortedPlayers. Player count < 2 (${playerTotals.length})`);
+    }
+    return output;
+  }
+
+
   return (
-    <>
-      <h1>Game Summary</h1>
-      <ul>
-        {playerTotals.map((player) =>
-          <li key={player.playerName}>{player.playerName} : {player.total}</li>
-        )}
-      </ul>
-    </>
+    <Container className='summary mb-4'>
+      <h1 className='d-flex justify-content-center title'>Game Summary</h1>
+      <div className='d-flex justify-content-center'>
+        <ul className='winners-list'>
+          {getSortedPlayers()}
+        </ul>
+      </div>
+
+      <hr />
+      <p>Graph stuff goes here</p>
+
+      <hr />
+
+      <div className='d-flex justify-content-center'>
+        <button className='btn btn-primary' onClick={onClickBackToScores}>Back to Scores</button>
+      </div>
+
+    </Container>
   )
 }
 
