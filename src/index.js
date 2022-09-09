8d29f370541827f9ef6315713dd40e7a3f357184
treyturley <81778542+treyturley@users.noreply.github.com>
2022-09-09 15:46:10 -0500

    implements flexbox sticky footer

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ import ReactDOM from 'react-dom/client';
 import Game from "./components/Game";
 import Header from './layouts/Header';
 import Footer from './layouts/Footer'
+import "./styles/Index.css"
 
 
 function App() {
   return (
     <>
       <React.StrictMode>
-        <Header />
-        <Game />
-        <Footer />
+        <div className="header">
+          <Header />
+        </div>
+        <div className="content">
+          <Game />
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
       </React.StrictMode>
     </>
   )
