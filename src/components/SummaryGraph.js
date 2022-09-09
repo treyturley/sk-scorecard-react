3dba826afaa8c01aae70087b2919c85e3c509a1d
treyturley <81778542+treyturley@users.noreply.github.com>
2022-09-09 18:01:32 -0500

    adds SummaryGraph component

---
+++ src/components/SummaryGraph.js
@@ -0,0 +1,109 @@
+import React from 'react'
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+function SummaryGraph({ playerTotals, scorecard }) {
+
+  const lineColors = [
+    '#332288',
+    '#88CCEE',
+    '#44AA99',
+    '#117733',
+    '#999933',
+    '#DDCC77',
+    '#CC6677',
+    '#882255',
+    '#AA4499',
+    '#CC3311'
+  ];
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Scores by Round',
+      },
+    },
+  };
+
+  const labels = [
+    'Round 1',
+    'Round 2',
+    'Round 3',
+    'Round 4',
+    'Round 5',
+    'Round 6',
+    'Round 7',
+    'Round 8',
+    'Round 9',
+    'Round 10'
+  ];
+
+  const data = {
+    labels,
+    datasets: []
+  };
+
+  class DataSet{
+    constructor(label, data, color){
+      this.label = label;
+      this.data = data;
+      this.backgroundColor = color;
+      this.borderColor = color;
+    }
+  }
+
+  playerTotals.forEach((player,index) => {
+    const roundTotals = [];
+    const roundScore = [];
+
+    scorecard.forEach((score) => {
+      if(score.playerName === player.playerName){
+        roundScore.push(score.roundTotal);
+      }
+    });
+
+    //calculate the score totals at each round
+    for(let x = 0; x < 10; x++){
+      let total = 0;
+      for(let y = 0; y <= x; y++){
+        total+=roundScore[y];
+      }
+      roundTotals.push(total);
+    }
+
+    data.datasets.push(new DataSet(player.playerName, roundTotals, lineColors[index]));
+    
+  });
+
+  return (
+    <Line options={options} data={data} />
+  )
+}
+
+export default SummaryGraph;
