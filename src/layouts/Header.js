08c3f03c6a8c27362543dc5c65887634bdca68c2
treyturley <81778542+treyturley@users.noreply.github.com>
2022-11-18 14:51:32 -0600

    fixes new game link and removes home link

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -10,8 +10,7 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/sk-scorecard-react/">New Game</Nav.Link>
+            <Nav.Link href="/skullking-scorecard">New Game</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
