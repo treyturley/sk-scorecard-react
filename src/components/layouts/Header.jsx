3531d811bf9946a3d4bca38a261e76abe649854d
treyturley <81778542+treyturley@users.noreply.github.com>
2022-12-12 11:53:44 -0600

    updates links in header and footer to use react router

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,18 +1,24 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Navbar bg='dark' variant='dark' expand='lg'>
       <Container>
-        <Navbar.Brand href='/skullking-scorecard'>
+        <Navbar.Brand as={Link} to='/skullking-scorecard'>
           Skull King Scorecard
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <Nav.Link href='/skullking-scorecard'>New Game</Nav.Link>
+            <Nav.Link as={Link} to='/skullking-scorecard/new-game'>
+              New Game
+            </Nav.Link>
+            <Nav.Link as={Link} to='/skullking-scorecard/join-game'>
+              Join Game
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
