2ddd7e462bfaa1d087fd9cc15514a56e0ee7806e
treyturley <81778542+treyturley@users.noreply.github.com>
2022-12-12 11:51:48 -0600

    adds not found pages to handle routing errors

---
+++ src/pages/GameNotFound.jsx
@@ -0,0 +1,14 @@
+const GameNotFound = () => {
+  return (
+    <div className='content sea-bg-image'>
+      <h1 className='mt-4 text-center'>
+        The Game you were looking for was not found!
+      </h1>
+      <h5 className='text-center'>
+        Please try creating a new game or joining an existing one.
+      </h5>
+    </div>
+  );
+};
+
+export default GameNotFound;
